a4d6a4cf3522c4262ce3d09178c228669d533332
Gladyson Rhuan <gladysonr04@gmail.com>
2026-01-21 21:31:44 -0300

    feat(payments): create PaymentHttpClient to communicate with Mercado Pago API

---
+++ app/Payments/Http/PaymentHttpClient.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Payments\Http;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+
+class PaymentHttpClient
+{
+    /**
+     * @throws ConnectionException
+     */
+    public function post(string $url, array $payload, array $headers = []): array
+    {
+        try {
+            return Http::withHeaders($headers)
+                ->post($url, $payload)
+                ->json();
+        } catch (ConnectionException $e) {
+            logger()->error($e->getMessage());
+            throw new ConnectionException($e->getMessage());
+        }
+    }
+}
