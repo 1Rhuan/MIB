d258965755e5848b5981f7039cba6fee8d6a3172
Gladyson Rhuan <gladysonr04@gmail.com>
2026-01-21 21:45:15 -0300

    feat(payments): bind PaymentGateway implementation in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Payments\Contracts\PaymentGateway;
+use App\Payments\Gateways\MercadoPagoGateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            PaymentGateway::class,
+            MercadoPagoGateway::class
+        );
     }
 
     /**
