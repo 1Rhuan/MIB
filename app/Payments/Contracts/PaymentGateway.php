2240bcadfcd36e88c72ee9ad3f45326612945235
Gladyson Rhuan <gladysonr04@gmail.com>
2026-01-22 19:20:11 -0300

    refactor(payments): rename getStatus to getPayment and align return types

--- app/Payments/Contracts/PaymentGateway.php
+++ app/Payments/Contracts/PaymentGateway.php
@@ -2,8 +2,13 @@
 
 namespace App\Payments\Contracts;
 
+use App\Payments\DTOs\PaymentResponseDto;
+
+/**
+ * @returns PaymentResponseDto
+ */
 interface PaymentGateway
 {
-    public function createPix(array $data);
-    public function getStatus(array $data);
+    public function createPix(array $data): PaymentResponseDto;
+    public function getPayment(string $paymentId): PaymentResponseDto;
 }
