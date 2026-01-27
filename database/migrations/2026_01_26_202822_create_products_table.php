53ca728e8e2d2645a117f11e51b47c445b8e5946
Gladyson Rhuan <gladysonr04@gmail.com>
2026-01-27 14:56:35 -0300

    feat(products): create products table and model with relationships

---
+++ database/migrations/2026_01_26_202822_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->ulid('reference')->unique();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+
+            $table->index('reference');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
