affd403803e13765dfd730abb4c797bcd370c537
Gladyson Rhuan <gladysonr04@gmail.com>
2026-01-27 14:56:05 -0300

    feat(orders): create orders table and model with relationships

---
+++ app/Models/Order.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * @method static create(array $array)
+ */
+class Order extends Model
+{
+    protected $fillable = [
+        'reference',
+        'user_id',
+        'status',
+        'total_amount',
+    ];
+
+    protected $casts = [
+        'total_amount' => 'decimal:2',
+        'status' => OrderStatus::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
