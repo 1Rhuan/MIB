25ef05f346488192db1f306e5625d07912e1f5cb
Gladyson Rhuan <gladysonr04@gmail.com>
2026-02-02 17:59:41 -0300

    feat(customers): create migration and model for customers table

---
+++ database/migrations/2026_02_01_150915_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('email')->unique();
+            $table->string('platform')->nullable();
+            $table->string('player_id')->nullable();
+            $table->timestamps();
+
+            $table->index('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
