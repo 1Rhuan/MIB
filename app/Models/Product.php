5325984f7844d54e9e28d85770202ce069a0a68a
Gladyson Rhuan <gladysonr04@gmail.com>
2026-02-02 19:35:55 -0300

    feat(products): add active scope and method to retrieve active product by ID

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -18,8 +19,20 @@ class Product extends Model
         'price' => 'decimal:2',
     ];
 
-    public function orderItems()
+    public function orderItems(): HasMany|Product
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', true);
+    }
+
+    public static function activeById(int $id): self
+    {
+        return self::active()
+            ->where('id', $id)
+            ->firstOrFail();
     }
 }
