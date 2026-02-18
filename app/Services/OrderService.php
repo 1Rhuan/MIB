6156131e437ad0af67080165f57280885aeb7c24
Gladyson Rhuan <gladysonr04@gmail.com>
2026-02-18 20:58:41 -0300

    feat(orders): refactor createOrder method and update OrderStatus enum with new values

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,86 +2,30 @@
 
 namespace App\Services;
 
-use App\DTOs\CreateOrderDto;
-use App\DTOs\PaymentRequestDto;
 use App\Models\Customer;
 use App\Models\Order;
 use App\Models\Product;
-use App\Payments\Contracts\PaymentGateway;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class OrderService
 {
-    public function __construct
-    (
-        private readonly PaymentGateway $paymentGateway
-    )
-    {}
-
-    public function createOrder(CreateOrderDto $orderDto): array
+    public function create(Product $product, Customer $customer, array $destinationAccount): Order
     {
-
-        $product = Product::findOrFail($orderDto->product_id);
-
-        $customer = Customer::firstOrCreate(
-            ['email' => $orderDto->email],
+        $order = Order::create(
             [
-                'first_name' => $orderDto->first_name,
-                'last_name' => $orderDto->last_name,
-            ]
-        );
-
-        $order = Order::create([
-            'customer_id' => $customer->id,
-            'total_amount' => 0,
-        ]);
-
-        $order->orderItems()->create(
-            [
+                'reference' => Str::ulid(),
+                'customer_id' => $customer->id,
+                'payment_method' => 'pix',
                 'product_id' => $product->id,
-                'product_name' => $product->name,
-                'unit_price' => $product->price,
-                'quantity' => 1,
-                'total_price' => $product->price,
+                'total_amount' => ($product->price - $product->discount),
             ]
         );
 
-        $order->update([
-            'total_amount' => $product->price,
+        $order->shipping()->create([
+            'platform' => $destinationAccount['platform'],
+            'player_id' => $destinationAccount['player_id'],
         ]);
 
-        $order->orderTarget()->create([
-            'platform' => $orderDto->platform,
-            'player_id' => $orderDto->player_id,
-            'nickname' => $orderDto->nickname,
-        ]);
-
-
-        $payment = $this->paymentGateway->createPix(new PaymentRequestDto(
-            firstName: $customer->first_name,
-            lastName: $customer->last_name,
-            email: $orderDto->email,
-            amount: (float) $order->total_amount,
-            orderReference: $order->reference,
-            productId: $product->id,
-            productName: $product->name,
-            productDescription: $product->description,
-            productPrice: (float) $product->price,
-            productQuantity: 1,
-            productCategory: $product->category,
-        ));
-
-        Log::alert("MP", $payment->toArray());
-
-        $order->payments()->create(
-            $payment->toArray(),
-        );
-
-        return [
-            'order' => $order,
-            "qr_code" => $payment->qrCode,
-            "qr_code_base64" => $payment->qrCodeBase64,
-            "mp_qr_code_link" => $payment->ticketUrl,
-        ];
+        return $order;
     }
 }
