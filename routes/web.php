d7304732cfcba23afea91b91f85f3424fffe0c7d
Gladyson Rhuan <gladysonr04@gmail.com>
2026-02-18 23:57:27 -0300

    feat(routes): restructure routes for checkout and payment functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class)->name('home');
+
+Route::prefix('/checkout')
+    ->group(function () {
+
+    Route::get('/{slug}', [CheckoutController::class, 'show'])
+        ->name('checkout.show');
+
+    Route::post('/process', [CheckoutController::class, 'process'])
+        ->name('checkout.process');
+});
+
+Route::prefix('/pagamento')->group(function () {
+    Route::get('/pedido/{reference}', [PaymentController::class, 'show'])
+        ->name('order.payment');
 });
