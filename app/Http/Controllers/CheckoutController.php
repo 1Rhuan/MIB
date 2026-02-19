b31477e59a92aedc85b0553426422db28ca2aea6
Gladyson Rhuan <gladysonr04@gmail.com>
2026-02-18 21:02:16 -0300

    feat(ui): implement CheckoutController for product checkout process

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ChekoutRequest;
+use App\Models\Product;
+use App\Services\CheckoutService;
+use Illuminate\View\View;
+
+class CheckoutController extends Controller
+{
+    public function __construct(
+        public CheckoutService $checkoutService,
+    ) {}
+
+    public function show(string $slug): View
+    {
+        $product = Product::where('slug', $slug)->firstOrFail();
+
+        return view('checkout.index', compact('product'));
+    }
+
+    public function process(ChekoutRequest $request)
+    {
+        $data = $request->validated();
+        $product = Product::where('slug', $data['product_slug'])->first();
+        $response = $this->checkoutService->process($product, $data);
+
+        return redirect()->route('order.payment', ['reference' => $response]);
+    }
+}
