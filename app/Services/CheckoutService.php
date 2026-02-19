db6b9e4803482d3c54996f411eac34a28308c0a3
Gladyson Rhuan <gladysonr04@gmail.com>
2026-02-18 21:02:26 -0300

    feat(service): add CheckoutService for processing product orders

---
+++ app/Services/CheckoutService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Customer;
+use App\Models\Product;
+use Illuminate\Support\Carbon;
+
+class CheckoutService
+{
+    public function __construct(
+        public OrderService $orderService,
+        public PaymentService $paymentService,
+    )
+    {
+        //
+    }
+
+    public function process(Product $product, array $data): string
+    {
+        $customer = Customer::firstOrCreate(
+            ['email' => $data['email']],
+            [
+                'first_name' => explode(' ', $data['name'])[0],
+                'last_name' => explode(' ', $data['name'])[count(explode(' ', $data['name'])) - 1],
+            ],
+        );
+
+        $order = $this->orderService->create($product, $customer, $data);
+        return $order->reference;
+    }
+}
